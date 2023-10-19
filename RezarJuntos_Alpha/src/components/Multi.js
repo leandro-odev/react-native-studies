bc38e40dfc9e6dbfa3d466c4b1ee85c6c20522b7
Leandro Wanderley <devleandromcz@gmail.com>
2023-10-19 18:38:53 -0300

    Update Multi.js
    Concertei o erro anterior

--- RezarJuntos_Alpha/src/components/Multi.js
+++ RezarJuntos_Alpha/src/components/Multi.js
@@ -1,20 +1,21 @@
 import React from "react";
-import { Text } from "  react-native";
+import { Text } from "react-native";
+import Estilo from "./estilo";
 
 export default function () {
     return (
-        <Text>Comp #Oficial</Text>
+        <Text style={Estilo.txtGG}>Comp #Oficial</Text>
     );
 }
 
 export function Comp1() {
     return (
-        <Text>Comp #01</Text>
+        <Text style={Estilo.txtGG}>Comp #01</Text>
     );
 }
 
 export function Comp2() {
     return (
-        <Text>Comp #02</Text>
+        <Text style={Estilo.txtGG}>Comp #02</Text>
     );
 }
