f2e76e8da238a99e41f1edf182ffd2a2ebfb5867
Leandro Wanderley <devleandromcz@gmail.com>
2023-10-20 14:51:22 -0300

    Update App.js

--- helloWorld/App.js
+++ helloWorld/App.js
@@ -1,32 +1,90 @@
-import React from 'react';
-import { View, SafeAreaView, Text, StatusBar, Image, StyleSheet } from 'react-native';
+  import React, {useState} from 'react';
+  import { SafeAreaView, View, Text, StyleSheet, Button, TextInput, StatusBar } from 'react-native';
 
-export default function App() {
-  return (
-    <SafeAreaView style={styles.Container}>
-      <StatusBar />
-      <Text style={styles.Vasco}>Vamos todos cantar de coração, a Cruz de Malta é o meu pendão, tu tens o nome do heroico português Vasco da Gama, a tua fama assim se fez. Tua imensa torcida é bem feliz, Norte-Sul, Norte-Sul deste Brasil. Tua estrela, na terra a brilhar Ilumina o mar. No atletismo, és um braço, o remo, és imortal, no futebol, és um traço de união Brasil-Portugal. No atletismo, és um braço, no remo, és imortal, no futebol, és um traço De união Brasil-Portugal Vamos todos cantar de coração A Cruz de Malta é o meu pendão! Tu tens o nome do heroico português Vasco da Gama, a tua fama assim se fez Tua imensa torcida é bem feliz Norte-Sul, Norte-Sul deste Brasil Tua estrela, na terra a brilhar Ilumina o mar No atletismo, és um braço No remo, és imortal No futebol, és um traço De união Brasil-Portugal No atletismo, és um braço No remo, és imortal No futebol, és um traço De união Brasil-Portugal</Text>
-      <Image style={styles.Image} source={require('./assets/vasco.png')}></Image>
-    </SafeAreaView>
-  );
-}
+  export default function App() {
+    const [numero, setNumero] = useState(0);
 
-const styles = StyleSheet.create({
-  Container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#000',
-  },
-  Image: {
-    height: 200,
-    width: 200,
-  },
-  Vasco: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    color: '#fff',
-    textAlign: 'justify',
-    marginHorizontal: 10,
+    const[nome,setNome] = useState('');
+    function pegaNome(){
+      alert("Olá " + nome);
+    }
+
+    function soma() {
+      setNumero(numero + 1);
+    }
+
+    function subtrair() {
+      setNumero(numero - 1);
+    }
+    
+
+    return (
+      <SafeAreaView style={styles.Container}>
+        <StatusBar />
+
+        <TextInput
+          style={styles.input}
+          placeholder='Digite um número'
+          onChangeText={(texto) => setNumero(Number(texto))}
+        />
+
+        <View style={styles.view1998}>
+          <Button
+            title='Soma'
+            onPress={soma}
+            style={styles.botao}
+          />
+          <Text style={styles.textoNumero}>{numero}</Text>
+          <Button
+            title='Subtrair'
+            onPress={subtrair}
+            style={styles.botao}
+          />
+        </View>
+
+      </SafeAreaView>
+    );
   }
-});
+
+  const styles = StyleSheet.create({
+    Container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    input: {
+      height: 40,
+      width: '80%',
+      borderWidth: 0.8,
+      margin: 2,
+      padding: 10,
+      fontSize: 15
+    },
+    botao: {
+      backgroundColor: '#121212',
+    },
+    view1998: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-around',
+      width: '50%',
+    },
+    textoNumero: {
+      textAlign: 'center',
+    },
+  });
+
+  1
+        {/* <TextInput
+          style={styles.input}
+          placeholder='Digite seu nome'
+          onChangeText={(texto) => setNome(texto)}
+        />
+
+        <Button 
+          title='Executar'
+          onPress={pegaNome}
+          style={styles.botao}
+        /> */}
+
