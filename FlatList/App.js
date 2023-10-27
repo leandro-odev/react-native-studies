545c15fc75b9f3cb8ce0fcc73889cb9b0aa9c4b7
leandrowqtc <devleandromcz@gmail.com>
2023-10-27 15:36:44 -0300

    FlatList testes

--- FlatList/App.js
+++ FlatList/App.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import { View, ScrollView, SafeAreaView, StyleSheet, StatusBar, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, ScrollView, SafeAreaView, StyleSheet, StatusBar, Text, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [feed, setFedd] = useState([
+    {id: '0', name: 'Dimitri Payet', age: 36, email: 'payet@vasco.com'},
+    {id: '1', name: 'Neymar Jr', age: 29, email: 'neymar@brasil.com'},
+    {id: '2', name: 'Lionel Messi', age: 33, email: 'messi@vasco.com'},
+    {id: '3', name: 'Cristiano Ronaldo', age: 36, email: 'cr7@vasco.com'},
+    {id: '4', name: 'Kylian Mbappé', age: 22, email: 'tartaruga@ninja.com'},
+    {id: '5', name: 'Luis Suárez', age: 34, email: 'mordida@gurisdogremio.com'},
+    {id: '6', name: 'Zlatan Ibrahimovic', age: 99999, email: 'email@zlatan.com'}
+  ])
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar/>
 
-      <ScrollView showsVerticalScrollIndicator={false}>
-
-        <View style={styles.box1}></View>
-        <View style={styles.box2}></View>
-        <View style={styles.box3}></View>
-        <View style={styles.box4}></View>
-        <View style={styles.box2}></View>
-
-      </ScrollView>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={feed}
+        // keyExtractor={item => item.id}
+        renderItem={(({item}) => <Pessoa data={item} />)}
+      />
 
     </SafeAreaView>
   );
@@ -25,27 +33,28 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingVertical: 20,
     backgroundColor: '#fff',
-    alignItems: 'center',
+  },
+  areaPessoa: {
+    flex: 1,
     justifyContent: 'center',
+    height: 200,
+    marginHorizontal: 10,
+    backgroundColor: '#121212',
   },
-  box1: {
-    height: 250,
-    width: 250,
-    backgroundColor: 'red',
-  },
-  box2: {
-    height: 250,
-    width: 250,
-    backgroundColor: 'blue',
-  },
-  box3: {
-    height: 250,
-    width: 250,
-    backgroundColor: 'green',
-  },
-  box4: {
-    height: 250,
-    width: 250,
-    backgroundColor: 'yellow',
+  text: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
+
   }
 });
+
+function Pessoa(props) {
+  return(
+    <View style={styles.areaPessoa}>
+      <Text style={styles.text}>Nome: {props.data.name}</Text>
+      <Text style={styles.text}>Idade: {props.data.age}</Text>
+      <Text style={styles.text}>Email: {props.data.email}</Text>
+    </View>
+  )
+}
